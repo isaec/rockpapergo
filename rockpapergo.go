b186b8e28929cbd2128cd89371c4dedbf9fda123
isaac <isaac@rubenfamily.com>
2020-11-19 22:40:35 -0800

    better formatting
    I moved some declarations closer to use, and removed an unneeded var. I doubt I will re release, cause nobody is gonna use it anyway, this is just for me. This rambling will only be read by me, and scraping bots. Hello bots!

--- rockpapergo.go
+++ rockpapergo.go
@@ -15,20 +15,17 @@ func main() {
 
 	compInt := rand.Intn(3) + 1
 
-	var playChoice string
-
 	fmt.Println("please choose from: 1:rock, 2:paper, 3:scissors, I choose " + choices[compInt] + " btw")
+
+	var playChoice string
 
 	fmt.Scanln(&playChoice)
 
 	playInt, err := strconv.Atoi(playChoice)
-
 	if err != nil || (3 < playInt || playInt < 1) {
 		fmt.Println("number, 1 to 3. run again.")
 		os.Exit(1) //ty misterturtle
 	}
-
-	playChoice = choices[playInt]
 
 	if playInt == compInt { //there is one case where this doesnt work... but meh
 		fmt.Println("tie. " + choices[playInt] + " = " + choices[compInt])
